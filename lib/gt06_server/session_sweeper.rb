56a80f526dbceed7a2b4a6bd16be0f0311652890
CoolElvis <elvisplus2@gmail.com>
2016-12-24 14:16:43 +0300

    session sweeper fix init params

--- lib/gt06_server/session_sweeper.rb
+++ lib/gt06_server/session_sweeper.rb
@@ -1,55 +1,57 @@
 require 'concurrent'
+module Gt06Server
+  class SessionSweeper
+    attr_reader :sessions , :info, :timeout, :interval, :logger
 
-class SessionSweeper
-  attr_reader :sessions , :info, :timeout, :interval, :logger
+    # @param sessions[Array<Session>]
+    def initialize(sessions, timeout = 60, interval: 30, logger: Logger.new(STDOUT))
+      @sessions = sessions
+      @timeout  = timeout || 60
+      @interval = interval || 30
+      @info     = {killed: 0, live:0, count:0}
+      @logger   = logger
+    end
 
-  def initialize(sessions, timeout = 60, interval: 30, logger: Logger.new(STDOUT))
-    @sessions = sessions
-    @timeout  = timeout
-    @interval = interval
-    @info     = {killed: 0, live:0, count:0}
-    @logger   = logger
-  end
+    def run
+      timer = Concurrent::TimerTask.new(execution_interval: @interval) do
+        time_now = Time.now
+        @sessions.each_pair do |key, session|
+          if (session.info[:last_received_at] + @timeout) < time_now
+            session.io.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1,0].pack('ii'))
+            session.io.close
 
-  def run
-    timer = Concurrent::TimerTask.new(execution_interval: @interval) do
-      time_now = Time.now
-      @sessions.each_pair do |key, session|
-        if (session.info[:last_received_at] + @timeout) < time_now
-          session.io.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1,0].pack('ii'))
-          session.io.close
+            @logger.debug "Session #{session.inspect} has been closed"
 
-          @logger.debug "Session #{session.inspect} has been closed"
+            @info[:killed] += 1
+            @sessions.delete(key)
+          end
+        end
 
-          @info[:killed] += 1
-          @sessions.delete(key)
+        @info[:live] = @sessions.size
+        @info[:count] += 1
+
+        @info
+      end
+
+      timer.add_observer(SessionSweeperObserver.new(@logger))
+      timer.execute
+    end
+
+    class SessionSweeperObserver
+      def initialize(logger)
+        @logger = logger
+      end
+
+      def update(time, result, exception)
+        if result
+          @logger.info "(#{time}) Execution successfully returned #{result}"
+        else
+          @logger.error "(#{time}) Execution failed with error #{exception}"
+          @logger.error "(#{time}) #{exception.backtrace}"
+          # Airbrake.notify(exception)
         end
       end
-
-      @info[:live] = @sessions.size
-      @info[:count] += 1
-
-      @info
     end
 
-    timer.add_observer(SessionSweeperObserver.new(@logger))
-    timer.execute
   end
-
-  class SessionSweeperObserver
-    def initialize(logger)
-      @logger = logger
-    end
-
-    def update(time, result, exception)
-      if result
-        @logger.info "(#{time}) Execution successfully returned #{result}"
-      else
-        @logger.error "(#{time}) Execution failed with error #{exception}"
-        @logger.error "(#{time}) #{exception.backtrace}"
-        # Airbrake.notify(exception)
-      end
-    end
-  end
-
 end
